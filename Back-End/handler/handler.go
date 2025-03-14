7f10e9d42788568f2af3eabc7bbcc7971d024de2
kishyassin <kishyassin@gmail.com>
2025-03-14 17:05:03 +0000

    feat: implement OCR handler and integrate Google Vision API for text extraction

--- Back-End/handler/handler.go
+++ Back-End/handler/handler.go
@@ -1,1 +1,46 @@
 package handler
+
+import (
+	"kishyassin/Salam-CV-Enhancer/utils"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func OCR(c *fiber.Ctx) error {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "File is required",
+		})
+	}
+
+	filePath := "./uploads/" + file.Filename
+
+	// Ensure the directory exists
+	if err := os.MkdirAll("./uploads", os.ModePerm); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to create directory",
+		})
+	}
+
+	if err := c.SaveFile(file, filePath); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to save file",
+		})
+	}
+
+	// Extract text with Google Vision API
+	text, err := utils.ExtractTextFromFile(filePath)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   "Unable to extract text",
+			"message": err.Error(),
+		})
+	}
+
+	// Return the extracted text
+	return c.JSON(fiber.Map{
+		"text": text,
+	})
+}
---
+++ Back-End/utils/file.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+
+	vision "cloud.google.com/go/vision/apiv1"
+	"google.golang.org/api/option"
+)
+
+func ExtractTextFromFile(filePath string) (string, error) {
+	ctx := context.Background()
+
+	// Use the JSON credentials file
+	client, err := vision.NewImageAnnotatorClient(ctx, option.WithCredentialsFile("./daring-fin-448723-d0-537da1216b22.json"))
+	if err != nil {
+		return "", fmt.Errorf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	fileBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read file: %v", err)
+	}
+
+	image, err := vision.NewImageFromReader(bytes.NewReader(fileBytes))
+	if err != nil {
+		return "", fmt.Errorf("failed to create image from reader: %v", err)
+	}
+
+	annotations, err := client.DetectTexts(ctx, image, nil, 1)
+	if err != nil {
+		return "", fmt.Errorf("failed to detect texts: %v", err)
+	}
+
+	if len(annotations) == 0 {
+		return "", nil
+	}
+
+	return annotations[0].Description, nil
+}
