2339ba3f97b7d1d9b28c029b44cb84f7cc0a54e9
kishyassin <kishyassin@gmail.com>
2025-03-14 13:48:14 +0000

    initialize project with Fiber framework and set up basic structure

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/gofiber/fiber/v2"
+	"kishyassin/Salam-CV-Enhancer/router"
+)
+
+func main() {
+	// Create a new Fiber app
+	app := fiber.New()
+
+	// Set up the routes with the database instance
+	router.SetupRoutes(app)
+
+	// Start the server
+	port := os.Getenv("APP_PORT") // Railway assigns this dynamically
+	if port == "" {
+		port = "3000" // Default fallback
+	}
+
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
